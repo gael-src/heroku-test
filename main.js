2e037fe5e222f42ebdb6d96c0bb74345310dfa71
gael-src <src.gael@gmail.com>
2021-10-28 11:33:23 +0200

    update

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ const app = express();
 const port = process.env.PORT || 3011;
 const url = `http://localhost:${port}`;
 
-app.get("/", (req, res) => res.send("secrets everglades"));
+app.get("/", (req, res) => res.send("secrets everglades git"));
 
 app.listen(port, () => console.log(`Listening on ${url}`));
